ed6663cd7db80e14a618fe2428c84c332d4eb11d
janfbacon <121216977+janfbacon@users.noreply.github.com>
2023-05-30 14:59:13 +0800

    Database Connection Class

---
+++ src/Connection/DBConn.java
@@ -0,0 +1,54 @@
+package Connection;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+public class DBConn {
+
+    static String password, user;
+    static String url;
+    static Connection conn;
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+        } catch (java.lang.ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, "Database Driver error. Please check the configuration!");
+        }
+
+        try {
+
+            String filePath = System.getProperty("user.dir") + "\\config.properties";
+
+            Properties properties = new Properties();
+
+            try {
+                properties.load(new FileInputStream(filePath));
+            } catch (IOException ex) {
+                Logger.getLogger(DBConn.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+            String port = properties.getProperty("port");
+            String host = properties.getProperty("host");
+            String database = properties.getProperty("database");
+
+            url = "jdbc:mysql://" + host + ":" + port + "/" + database;
+            user = "root";
+            password = "";
+            conn = DriverManager.getConnection(url, user, password);
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+
+        return conn;
+    }
+}
